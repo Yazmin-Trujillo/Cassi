b726c88ebef8c3105da03c33aa51919f5c19db84
Yazmín Trujillo López <97549396+Yazmin-Trujillo@users.noreply.github.com>
2026-01-04 14:28:00 -0600

    con /

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: "cassi",
-  assetPrefix: "cassi",
+  basePath: "/cassi",
+  assetPrefix: "/cassi",
 };
 
 export default nextConfig;
