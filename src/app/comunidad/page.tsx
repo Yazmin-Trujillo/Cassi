e88104e4e4944185c3b4bbe3d3ca30e9880b8311
Yazmín Trujillo López <97549396+Yazmin-Trujillo@users.noreply.github.com>
2026-06-13 18:19:14 -0600

    error

--- src/app/comunidad/page.tsx
+++ src/app/comunidad/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { Button } from "../components/atomos/button";
 import { Card } from "../components/moleculas/card";
-import { EtiquetaDeCategorias } from "../utils";
 
 export default function Comunidad() {
   return (
